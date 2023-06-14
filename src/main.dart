dcf53acdaa832e8e8431cd73ea062a6823616f73
Hayri Bakici <hayri.bakici@gmail.com>
2023-06-15 01:21:43 +0200

    adds usage

--- src/main.dart
+++ src/main.dart
@@ -9,17 +9,34 @@ const start = 'startswith';
 void main(List<String> args) async {
   exitCode = 0; // presume success
   final parser = ArgParser()
-    ..addFlag(similar, negatable: true, abbr: 's')
-    ..addFlag(start, negatable: true, abbr: 'w')
-    ..addFlag(baseform, negatable: true, abbr: 'b');
+    ..addFlag(similar,
+        negatable: false,
+        help: 'Return similar spelled words, helpful for misspellings',
+        abbr: 's')
+    ..addFlag(start,
+        negatable: false,
+        help: 'Return words that have the same starting letters as the query',
+        abbr: 'w')
+    ..addFlag(baseform, negatable: false, abbr: 'b');
 
+  if (args.isEmpty) {
+    print(parser.usage);
+    return;
+  }
   ArgResults argResults = parser.parse(args);
   final query = argResults.rest[0];
+  var withSimilar = argResults[similar] as bool;
+  var withStart = argResults[start] as bool;
+  var withBaseForm = argResults[baseform] as bool;
 
   var ot = OpenThesaurus.create();
-  var response = await ot.getWith(query,
-      similar: argResults[similar] as bool,
-      startsWith: argResults[start] as bool,);
+  var response = await ot.getWith(
+    query,
+    similar: withSimilar,
+    startsWith: withStart,
+    baseform: withBaseForm
+  );
+
   for (var syn in response.synonymSet!) {
     print(syn.terms?.map((e) => e.term).toList());
   }
