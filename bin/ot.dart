7048d1424ca5d4d5d036f5027ed95b1530f935fa
Hayri Bakici <hayri.bakici@gmail.com>
2023-06-16 22:48:18 +0200

    add licence heaader

--- bin/ot.dart
+++ bin/ot.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2023, hayribakici. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 import '../src/main.dart' as ot;
 
 Future<void> main(List<String> args) async => ot.main(args);
--- src/main.dart
+++ src/main.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2023, hayribakici. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// A dart library for interfacing with the API of openthesaurus.de.
 import 'package:chalkdart/chalk_x11.dart';
 import 'package:openthesaurus/openthesaurus.dart';
 import 'package:args/args.dart';
